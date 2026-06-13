1381cdd90cc1cc813d3068da4cfaabe1ca44b871
Wesley-gomes1 <wesleybatistagomes.bsb@gmail.com>
2026-06-12 21:20:21 -0300

    atualizaçao

---
+++ programa01.py
@@ -0,0 +1,9 @@
+nome = "Wesley" # str - stringer
+ultimo_nome = "Gomes" # str - stringer
+Idade = 20 # int - inteiro # int - inteiro
+Altura = 1.77 # - float
+Maior_de_idade = True # bool - booleano : True/false
+print("Olá", nome)
+print (f"Ola {nome}")
+
+print(f"Olá {nome} {ultimo_nome} você tem {Idade} anos, sua altura é {Altura}, você é maior de idade? {Maior_de_idade}!")
