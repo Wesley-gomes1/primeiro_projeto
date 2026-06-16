2cc8302ddfbf87f297bbdf70db61093ead1df29e
Wesley-gomes1 <wesleybatistagomes.bsb@gmail.com>
2026-06-15 21:24:46 -0300

    entrada de dados pelo terminal e operacoes matemáticas

---
+++ programa02.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ") #str
+
+ano_de_nasc = int(input (f"Prazer em te conhecer {nome}!, digite o ano de seu nascimento: ")) #int
+idade = 2026 - ano_de_nasc
+print(f"Parabéns por seus {idade} anos!") #str
---
+++ programa03.py
@@ -0,0 +1,24 @@
+# Calculadora python
+num1 = int(input("digite o primeiro numero: "))
+num2 = int(input("digite o segundo numero: "))
+soma = num1 + num2
+subtracacao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+divisao_inteira = num1 //num2
+resto_divisao = num1 % num2
+potenciacao = num1  ** num2
+
+print(f"A soma de {num1} e {num2} é igual a = {soma}")
+
+print(f"A subtracao de {num1} e {num2} é igual a = {subtracacao}")
+
+print(f"A multiplicacao de {num1} e {num2} é igual a = {multiplicacao}")
+
+print(f"A divisao de {num1} e {num2} é igual a = {divisao}")
+
+print(f"A divisao inteira de {num1} e {num2} é igual a = {divisao_inteira}")
+
+print(f"O resto da divisao {num1} e {num2} é igual a = {resto_divisao}")
+
+print(f"A pontenciacao de {num1} e {num2} é igual a = {potenciacao}")
