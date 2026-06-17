800357e7549d3859364f2f3caa1043697be79b3d
Wesley-gomes1 <wesleybatistagomes.bsb@gmail.com>
2026-06-16 21:29:37 -0300

    aprendendo condicional

---
+++ estrutura_condicional.py
@@ -0,0 +1,15 @@
+# Casar ou Comprar uma Bicicleta ?
+print("casar ou comprar uma bicicleta? ")
+resposta = input("você está gordo? s/n ")
+if  resposta == "s":
+    quer_emagrecer = input("Você quer eagrecer? s/n ")
+    if quer_emagrecer == "s":
+     print("Compre uma bicicleta!")
+    else:
+     print("Então case!")
+else:
+    quer_engordar = input("você quer engordar? s/n ")
+    if quer_engordar == "s":
+        print("Então case!")
+    else:
+        print("então compre uma bicicleta!")
---
+++ media_aluno.py
@@ -0,0 +1,23 @@
+from selectors import SelectSelector
+
+nota1 = float(input("Digite sua primeira nota: "))
+
+nota2 = float(input("Digite a segunda nota: "))
+
+nota3 = float(input("Digite sua terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+if media >= 7 :
+    print(f"Aluno aprovado com média {media: .2f}" )
+
+elif media >= 3 and media < 7:
+    print (f"Aluno em recuperação com média {media: .2f}")
+    fez_recuperacao = input("Aluno já fez a recuperação? s/n: ")
+    if fez_recuperacao == "s":
+        nota_recuperacao = float(input("digite a nota da recuperação: "))
+        if nota_recuperacao >= 5:
+            print("Aluno aprovado pela recuperaçãa")
+    else:
+        print("AQluno não obteve nota suficiente para ser aprovado após a recuperação.")
+else:
+    print(f"Aluno reprovado com média {media: .2f}" )
