cda51a1d5d05c6e3ba1b986d2ba052aad2c1ae61
Wesley-gomes1 <wesleybatistagomes.bsb@gmail.com>
2026-06-17 21:15:28 -0300

    Atualização

--- media_aluno.py
+++ media_aluno.py
@@ -12,12 +12,14 @@ if media >= 7 :
 
 elif media >= 3 and media < 7:
     print (f"Aluno em recuperação com média {media: .2f}")
-    fez_recuperacao = input("Aluno já fez a recuperação? s/n: ")
+    fez_recuperacao = input("Aluno já fez  a recuperação? s/n: ")
     if fez_recuperacao == "s":
         nota_recuperacao = float(input("digite a nota da recuperação: "))
         if nota_recuperacao >= 5:
             print("Aluno aprovado pela recuperaçãa")
+        else:
+            print("Aluno não obteve nota suficiente para ser aprovado após a recuperação.")
     else:
-        print("AQluno não obteve nota suficiente para ser aprovado após a recuperação.")
+        print("Aluno ainda não fez a recuperação")
 else:
     print(f"Aluno reprovado com média {media: .2f}" )
---
+++ par_ou_impar.py
@@ -0,0 +1,12 @@
+num = int(input("Digite um número: "))
+
+if num % 2 == 0:
+        print("Seu número é par!")
+
+else:
+        print("Seu número é ímpar!")
+
+
+
+
+
