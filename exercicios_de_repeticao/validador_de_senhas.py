5ee7afa5c0951b14998533844374ba6f4b484803
Wesley-gomes1 <wesleybatistagomes.bsb@gmail.com>
2026-06-25 19:26:48 -0300

    atualizacao

--- exercicios_de_repeticao/validador_de_senhas.py
+++ exercicios_de_repeticao/validador_de_senhas.py
@@ -1,6 +1,6 @@
 senha_correta = 123456
 
-for i in range (3):
+for i in range (2,-1,-1):
     senha_digitada = int(input("Digite sua senha: "))
     if senha_digitada == senha_correta:
         print("Acesso Permitido")
